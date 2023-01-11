2935711e23f72e4da9f404218f8c8491e5fd47bd
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-11 11:17:35 +0700

    html for about page

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+// import react
+import React from 'react';
+
+// create component
+class About extends React.Component{
+  render() {
+    return (
+        <section id="about">
+            <img src="" alt="me" width="200" />
+            <div>
+                <span>Hello I'm</span>
+                <h1>Ester</h1>
+                <h2>Lorem ipsum dolor sit amet consectetur</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+            </div>
+        </section>
+    )
+  }
+}
+
+// export react
+export default About;
