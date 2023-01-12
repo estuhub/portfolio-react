23035c51e9e4880f0b7db5008b86d4e21d038a32
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-12 11:38:34 +0700

    html for skills page

---
+++ src/pages/Skills.js
@@ -0,0 +1,48 @@
+// import react
+import React from 'react';
+
+// create component
+class Skills extends React.Component{
+  render() {
+    return (
+        <section id="skills">
+            <h2>Services</h2>
+            <div>
+                <h2>Web Development</h2>
+                <img src="" alt="Tortuga Coders" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <h2>Skills</h2>
+                <img src="" alt="React" />
+                <img src="" alt="JavaScript" />
+                <img src="" alt="HTML" />
+                <img src="" alt="CSS" />
+            </div>
+            <div>
+                <h2>Data Analytics</h2>
+                <img src="" alt="adidas" />
+                <img src="" alt="Unilever" />
+                <img src="" alt="P&G" />
+                <img src="" alt="Nielsen" />
+                <h2>Skills</h2>
+                <img src="" alt="Excel" />
+                <img src="" alt="SAP" />
+                <img src="" alt="PowerBI" />
+                <img src="" alt="QlikView" />
+            </div>
+            <div>
+                <h2>Hospitality</h2>
+                <img src="" alt="Pangea Pod Hotel" />
+                <img src="" alt="Araxi Restaurant & Oyster Bar" />
+                <img src="" alt="The Keg" />
+                <h2>Skills</h2>
+                <img src="" alt="Proceso de mejora continua" />
+                <img src="" alt="Training" />
+            </div>
+        </section>
+    )
+  }
+}
+
+// export react
+export default Skills;
