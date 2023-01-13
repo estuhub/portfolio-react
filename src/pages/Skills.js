6566ee222978388443577fe2a4ceec158ad40026
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-13 14:40:28 +0700

    css for skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -6,38 +6,35 @@ class Skills extends React.Component{
   render() {
     return (
         <section id="skills">
-            <h2>Services</h2>
-            <div>
-                <h2>Web Development</h2>
-                <img src="" alt="Tortuga Coders" />
-                <img src="" alt="" />
-                <img src="" alt="" />
-                <h2>Skills</h2>
-                <img src="" alt="React" />
-                <img src="" alt="JavaScript" />
-                <img src="" alt="HTML" />
-                <img src="" alt="CSS" />
-            </div>
-            <div>
-                <h2>Data Analytics</h2>
-                <img src="" alt="adidas" />
-                <img src="" alt="Unilever" />
-                <img src="" alt="P&G" />
-                <img src="" alt="Nielsen" />
-                <h2>Skills</h2>
-                <img src="" alt="Excel" />
-                <img src="" alt="SAP" />
-                <img src="" alt="PowerBI" />
-                <img src="" alt="QlikView" />
-            </div>
-            <div>
-                <h2>Hospitality</h2>
-                <img src="" alt="Pangea Pod Hotel" />
-                <img src="" alt="Araxi Restaurant & Oyster Bar" />
-                <img src="" alt="The Keg" />
-                <h2>Skills</h2>
-                <img src="" alt="Proceso de mejora continua" />
-                <img src="" alt="Training" />
+            <h2 className="skills-header">Skills</h2>
+            <div className="skills-wrapper">
+                <div className="skills-card">
+                    {/* <div>
+                        <span className="skill-icon">Icon</span>            
+                    </div> */}
+                    <div>
+                        <h3 className="skill-header">Web Development</h3>
+                        <p className="skill-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+                    </div>
+                </div>
+                <div className="skills-card">
+                    {/* <div>
+                        <span className="skill-icon">Icon</span>            
+                    </div> */}
+                    <div>
+                        <h3 className="skill-header">Data Analytics</h3>
+                        <p className="skill-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+                    </div>
+                </div>
+                <div className="skills-card">
+                    {/* <div>
+                        <span className="skill-icon">Icon</span>            
+                    </div> */}
+                    <div>
+                        <h3 className="skill-header">Hospitality</h3>
+                        <p className="skill-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
