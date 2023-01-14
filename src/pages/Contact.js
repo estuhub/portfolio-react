9548c6222b9c311d83a13ce154f0c9dd295f10b2
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-14 13:19:21 +0700

    css for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,24 +7,26 @@ class Contact extends React.Component{
     return (
         <section id="contact">
             <h2>Get In Touch With Me</h2>
-            <form>
-                <div>
-                    <label>Name</label>
-                    <input type="Text" name="sender-name" placeholder="Enter Your Name" required />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" name="sender-email" placeholder="Enter Your Email" required />
-                </div>
-                <div>
-                    <label>Message</label>
-                    <textarea rows="10" name="message" placeholder="Enter Your Message" required ></textarea>
-                </div>
-                <div>
-                    <button>Submit</button>
-                </div>
-            </form>
-    </section>
+            <div className="contact-form-container contact-form">
+                <form>
+                    <div className="form-control">
+                        <label>Name</label>
+                        <input type="Text" name="sender-name" placeholder="Enter Your Name" className="input-field" required/>
+                    </div>
+                    <div className="form-control">
+                        <label>Email</label>
+                        <input type="email" name="sender-email" placeholder="Enter Your Email" className="input-field" required />
+                    </div>
+                    <div className="form-control">
+                        <label>Message</label>
+                        <textarea rows="10" name="message" placeholder="Enter Your Message" className="input-field" required ></textarea>
+                    </div>
+                    <div>
+                        <button class="submit-btn">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </section>
     )
   }
 }
