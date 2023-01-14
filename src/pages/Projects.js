3e7ca604519f9892f7d09fdfbe51608479dfbbf8
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-14 13:04:31 +0700

    css for projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,24 +6,26 @@ class Projects extends React.Component{
   render() {
     return (
         <section id="projects">
-            <h2>Some of my Recent Projects</h2>
-            <div>
-                <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d2abd662597191.5a9589b09ddf5.jpg" alt="Google Logo" class="project-logo" />
-                <h3>Google Search Clone</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut quibusdam fuga error nesciunt eaque saepe esse itaque voluptatibus praesentium architecto, distinctio iure doloribus nostrum. Molestias nulla enim nam amet.</p>
-                <a href="https://github.com/">Check it out!</a>
-            </div>
-            <div>
-                <img src="https://mumbrella.com.au/wp-content/uploads/2015/01/unnamed-1.jpg" alt="AirBnB Logo" class="project-logo" />
-                <h3>Airbnb Clone</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut quibusdam fuga error nesciunt eaque saepe esse itaque voluptatibus praesentium architecto, distinctio iure doloribus nostrum. Molestias nulla enim nam amet.</p>
-                <a href="https://github.com/">Check it out!</a>
-            </div>
-            <div>
-                <img src="https://images-platform.99static.com//0CyRDgaZCtmsodP0_44A_kMQj2k=/129x79:877x827/fit-in/500x500/99designs-contests-attachments/91/91806/attachment_91806333" alt="FMT Logo" class="project-logo" />
-                <h3>Follow My Trail</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aut quibusdam fuga error nesciunt eaque saepe esse itaque voluptatibus praesentium architecto, distinctio iure doloribus nostrum. Molestias nulla enim nam amet.</p>
-                <a href="https://github.com/">Check it out!</a>
+            <h2 className="projects-title">Some of my Recent Projects</h2>
+            <div className="projects-container">
+                <div className="project-container project-card">
+                    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d2abd662597191.5a9589b09ddf5.jpg" alt="Google Screenshot" class="project-pic" />
+                    <h3 className="project-title">Google Search Clone</h3>
+                    <p className="project-details">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas ratione vel inventore labore commodi modi quos culpa aut saepe! Alias!</p>
+                    <a href="https://github.com/" target="_blank" class="project-link">Check it out!</a>
+                </div>
+                <div className="project-container project-card">
+                    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d2abd662597191.5a9589b09ddf5.jpg" alt="Airbnb Screenshot" class="project-pic" />
+                    <h3 className="project-title">Airbnb Clone</h3>
+                    <p className="project-details">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas ratione vel inventore labore commodi modi quos culpa aut saepe! Alias!</p>
+                    <a href="https://github.com/" target="_blank" class="project-link">Check it out!</a>
+                </div>
+                <div className="project-container project-card">
+                    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/d2abd662597191.5a9589b09ddf5.jpg" alt="FMT Screenshot" class="project-pic" />
+                    <h3 className="project-title">Follow My Trail</h3>
+                    <p className="project-details">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas ratione vel inventore labore commodi modi quos culpa aut saepe! Alias!</p>
+                    <a href="https://github.com/" target="_blank" class="project-link">Check it out!</a>
+                </div>
             </div>
         </section>
     )
