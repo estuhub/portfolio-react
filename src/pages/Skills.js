1ad3c4761044f8ac88dd1f3a2827fe0a37a8868a
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-16 10:38:08 +0700

    add text to skills page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -6,7 +6,7 @@ class Skills extends React.Component{
   render() {
     return (
         <section id="skills">
-            <h2 className="skills-header">Skills</h2>
+            <h2 className="skills-header">My Top Skills</h2>
             <div className="skills-wrapper">
                 <div className="skills-card">
                     {/* <div>
@@ -14,7 +14,7 @@ class Skills extends React.Component{
                     </div> */}
                     <div>
                         <h3 className="skill-header">Web Development</h3>
-                        <p className="skill-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+                        <p className="skill-text">Currently working as a mentor for web dev students. Experience in developing web page efficiently using React, JavaScript, HTML and CSS.</p>
                         <h4 className="skill-place">Companies I worked with</h4>
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-tortuga_d3snuo.png" alt="logo" className="skill-logo" />
                     </div>
@@ -25,7 +25,7 @@ class Skills extends React.Component{
                     </div> */}
                     <div>
                         <h3 className="skill-header">Data Analytics</h3>
-                        <p className="skill-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+                        <p className="skill-text">Worked as Data Analyst with +10 years of experience in the retail and multinational consumer goods companies. Extensively worked developing decision-making dashboards and presentations. Expert using Microsoft Excel, Power BI and SAP.</p>
                         <h4 className="skill-place">Companies I worked with</h4>
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-adidas_dv5zn4.webp" alt="logo" className="skill-logo" />
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-nielsen_crk0c5.png" alt="logo" className="skill-logo" />
@@ -39,7 +39,7 @@ class Skills extends React.Component{
                     </div> */}
                     <div>
                         <h3 className="skill-header">Hospitality</h3>
-                        <p className="skill-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
+                        <p className="skill-text">Worked as F&B Supervisor and Server over 4 years in boutique bars and fine dining restaurants. Successfully manage F&B inventories working in a fast-paced environment. Highly capable of solving problems under pressure and deliver a quality service.</p>
                         <h4 className="skill-place">Companies I worked with</h4>
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-keg_kytrp9.png" alt="logo" className="skill-logo" />
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-araxi_sm4twy.jpg" alt="logo" className="skill-logo" />
