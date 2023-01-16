575d136533f11b5d3fb297ae2d2547144de89e2f
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-16 10:36:23 +0700

    convert home to hero-image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,9 @@ class About extends React.Component{
     return (
         <section id="home">
             <div className="bio">
-                <span>Hello I'm</span>
-                <h1 className="bio-name">Ester</h1>
-                <h2 className="bio-title">Lorem ipsum dolor sit amet consectetur</h2>
-                <p className="bio-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, maxime doloribus dolorem eius eum animi reiciendis autem omnis magni tempora aspernatur praesentium consequuntur consectetur at doloremque quas iste illum voluptatum.</p>
-            </div>
-            <div>
-                <img src="https://iso.500px.com/wp-content/uploads/2020/04/stock-photo-self-portraits-1013620750.jpg" alt="me" width="200" />
+                <h1 className="bio-name">I am Ester</h1>
+                <p className="bio-title">And I'm a Web Developer</p>
+                <p className="bio-text bio-btn">Contact me</p>
             </div>
         </section>
     )
