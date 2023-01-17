59cdec72484566e03b7cdedeea4c75a7ba35d8bd
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-17 19:30:42 +0700

    add text to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,10 +7,9 @@ class About extends React.Component{
     return (
         <section id="about">
             <h2>About Me</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>I graduated top of the class Oct'22 as Full Stack Web Dev after completing 11 weeks of Bootcamp in Koh Phangan - Thailand. I also have an analytical and numerical background.</p>
+            <p>I'm interested in web design and single page applications. I am currently working on a number of personal projects and starting a mentor position with the same Bootcamp I graduated from.</p>
+            <p>I love travelling and living in different countries. In the past 15 years, I've lived in Argentina, Germany, Panama, Canada and Thailand. I am fast learner and very curious about new technologies, always looking to improve my skills.</p>
         </section>
     )
   }
