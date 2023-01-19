06e6353192bccb2550f562934c55bd5e9ff1b784
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-19 18:35:23 +0700

    spell check

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ class About extends React.Component{
     return (
         <section id="about">
             <h2 className="header">About Me</h2>
-            <p>I graduated top of the class Oct'22 as Full Stack Web Dev after completing 11 weeks of Bootcamp in Koh Phangan - Thailand. I also have an analytical and numerical background. I'm interested in web design and single page applications. I am currently working on a number of personal projects and starting a mentor position with the same Bootcamp I graduated from. I love travelling and living in different countries. In the past 15 years, I've lived in Argentina, Germany, Panama, Canada and Thailand. I am fast learner and very curious about new technologies, always looking to improve my skills.</p>
+            <p>In October 2022, I graduated top of class from an 11-week Full Stack Web Development bootcamp in Koh Phangan, Thailand. Currently, I am working on a number of personal projects whilst starting a mentoring position with the same Bootcamp I graduated from. I love to travel and live in different countries; in the past 15 years I've lived in Argentina, Germany, Panama, Canada, and Thailand. With an analytical and numerical background, I am a fast learner and always looking to improve my skills. I am curious about new technologies and particularly interested in web design and single page applications.</p>
         </section>
     )
   }
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -12,7 +12,7 @@ class Skills extends React.Component{
                 <div className="skills-card">
                     <div>
                         <h3 className="skill-header">Web Development</h3>
-                        <p className="skill-text">Currently working as a mentor for a coding bootcamp. Experience in developing web page efficiently using React, JavaScript, HTML and CSS.</p>
+                        <p className="skill-text">Currently working as a mentor for a coding bootcamp. Experience in developing web pages efficiently using React, JavaScript, HTML and CSS.</p>
                         <h4 className="skill-place">Companies I worked with</h4>
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-tortuga_d3snuo.png" alt="logo" className="skill-logo" />
                     </div>
@@ -20,7 +20,7 @@ class Skills extends React.Component{
                 <div className="skills-card">
                     <div>
                         <h3 className="skill-header">Data Analytics</h3>
-                        <p className="skill-text">Worked with data analysis for more than 10 years in the retail and multinational consumer goods companies. Extensively worked developing decision-making dashboards and presentations. Expert using Microsoft Excel, Power BI and SAP.</p>
+                        <p className="skill-text">Worked with data analysis for more than 10 years in retail and multinational consumer goods companies. Extensively worked developing decision-making dashboards and presentations. Expert using Microsoft Excel, Power BI and SAP.</p>
                         <h4 className="skill-place">Companies I worked with</h4>
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-adidas_dv5zn4.webp" alt="logo" className="skill-logo" />
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-nielsen_crk0c5.png" alt="logo" className="skill-logo" />
@@ -31,7 +31,7 @@ class Skills extends React.Component{
                 <div className="skills-card">
                     <div>
                         <h3 className="skill-header">Hospitality</h3>
-                        <p className="skill-text">Worked as F&B Supervisor and Server over 4 years in boutique bars and fine dining restaurants. Successfully manage F&B inventories working in a fast-paced environment. Highly capable of solving problems under pressure and deliver a quality service.</p>
+                        <p className="skill-text">Worked as F&B Supervisor and Server over 4 years in boutique bars and fine dining restaurants. Successfully managed F&B inventories working in a fast-paced environment. Highly capable of solving problems under pressure and delivering a quality service.</p>
                         <h4 className="skill-place">Companies I worked with</h4>
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-keg_kytrp9.png" alt="logo" className="skill-logo" />
                         <img src="https://res.cloudinary.com/dr28hft1z/image/upload/v1673832654/portfolio-react/logo-araxi_sm4twy.jpg" alt="logo" className="skill-logo" />
