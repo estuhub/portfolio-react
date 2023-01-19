b3cef13b04b96232baf2818d3f1ad10aaefcf242
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-19 15:06:29 +0700

    add about-skills-projects-contact to deployment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import Nav from './components/Nav'
 import Home from './pages/Home';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 // create component
 class App extends React.Component{
@@ -10,6 +14,10 @@ class App extends React.Component{
       <div>
         <Nav />
         <Home />
+        <About />  
+        <Skills />  
+        <Projects />  
+        <Contact />  
       </div>
     )
   }
