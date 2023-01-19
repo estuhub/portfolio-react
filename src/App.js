518b4a6a9c662f6e6a3357608eee7541a40a8543
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-19 12:46:00 +0700

    Revert "test for infinite scrolling"
    This reverts commit f4c8575ae25b71d2c165a54488673e12a9d03b1b.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 // import react
 import React from 'react';
-import Nav from './components/Nav';
-import Home from './pages/Home';
-import About from './pages/About';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 
 // create component
 class App extends React.Component{
   render() {
-    return (
-      <div> 
-        <Nav />
-        <Home />  
-        <About />  
-        <Skills />  
-        <Projects />  
-        <Contact />  
-      </div>
-    )
+    return <p>Hello World</p>
   }
 }
 
