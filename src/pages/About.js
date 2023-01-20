3afc725d06006330ec4abcdbef794ec095fdc199
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-20 14:26:52 +0700

    update style for submit button

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ class About extends React.Component{
     return (
         <section id="about">
             <h2 className="header">About Me</h2>
-            <p>In October 2022, I graduated top of class from an 11-week Full Stack Web Development bootcamp in Koh Phangan, Thailand. Currently, I am working on a number of personal projects whilst starting a mentoring position with the same Bootcamp I graduated from. I love to travel and live in different countries; in the past 15 years I've lived in Argentina, Germany, Panama, Canada, and Thailand. With an analytical and numerical background, I am a fast learner and always looking to improve my skills. I am curious about new technologies and particularly interested in web design and single page applications.</p>
+            <p>In October 2022, I graduated top of class from an 11-week Full Stack Web Development bootcamp in Koh Phangan, Thailand. Currently, I am working on a number of personal projects whilst starting a mentoring position with the same Bootcamp I graduated from. I love to travel and live in different countries; in the past 15 years I've lived in Argentina, Germany, Panama, Canada and Thailand. With an analytical and numerical background, I am a fast learner and always looking to improve my skills. I am curious about new technologies and particularly interested in web design and single page applications.</p>
         </section>
     )
   }
