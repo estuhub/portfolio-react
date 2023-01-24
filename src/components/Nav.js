8a5a11dbe453b913c162efc93122ba2f74abbfed
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-24 18:12:08 +0700

    align logo and burguer-menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,19 +6,21 @@ const Nav = () => {
     const [navOpen, setNavOpen] = useState(false);
     return (
         <nav>
-            <p>I am Ester</p>
-            <div className="burguer-container" onClick={() => setNavOpen(!navOpen)}>
-                <div className={navOpen ? "change burguer-bar1" : "burguer-bar1"}></div>
-                <div className={navOpen ? "change burguer-bar2" : "burguer-bar2"}></div>
-                <div className={navOpen ? "change burguer-bar3" : "burguer-bar3"}></div>
+            <p>Ester Lin</p>
+            <div className="nav-wrap">
+                <div className="burguer-container" onClick={() => setNavOpen(!navOpen)}>
+                    <div className={navOpen ? "change burguer-bar1" : "burguer-bar1"}></div>
+                    <div className={navOpen ? "change burguer-bar2" : "burguer-bar2"}></div>
+                    <div className={navOpen ? "change burguer-bar3" : "burguer-bar3"}></div>
+                </div>
+                <ul className={navOpen ? "" : "navigation"}>
+                    <li><a href="#home" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Home</a></li>
+                    <li><a href="#about" className="nav-link" onClick={() => setNavOpen(!navOpen)}>About</a></li>
+                    <li><a href="#skills" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Skills</a></li>
+                    <li><a href="#projects" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Projects</a></li>
+                    <li><a href="#contact" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Contact</a></li>
+                </ul>
             </div>
-            <ul className={navOpen ? "" : "navigation"}>
-                <li><a href="#home" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Home</a></li>
-                <li><a href="#about" className="nav-link" onClick={() => setNavOpen(!navOpen)}>About</a></li>
-                <li><a href="#skills" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Skills</a></li>
-                <li><a href="#projects" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Projects</a></li>
-                <li><a href="#contact" className="nav-link" onClick={() => setNavOpen(!navOpen)}>Contact</a></li>
-            </ul>
         </nav>
     )
 }
