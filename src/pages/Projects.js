c93b4d1f682385aee05af4df8c52ea2acdce793b
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-28 17:57:04 +0700

    correct class for React

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ class Projects extends React.Component{
             <h2 className="header">Some of my Recent Projects</h2>
             <div className="projects-container">
                 <div className="project-container project-1">
-                    <a href="https://github.com/estuhub/google-search-react" target="_blank" rel="noreferrer" class="project-link">
+                    <a href="https://github.com/estuhub/google-search-react" target="_blank" rel="noreferrer" className="project-link">
                         <div className="project-overlay">
                             <h3 className="project-title">Google Search Clone</h3>
                             <p className="project-details">React - JSX - CSS - Express.js - Node.js - API - Axios - MongoDB</p>
@@ -17,7 +17,7 @@ class Projects extends React.Component{
                     </a>
                 </div>
                 <div className="project-container project-2">
-                    <a href="https://github.com/estuhub/airbnb-mvc" target="_blank" rel="noreferrer" class="project-link">
+                    <a href="https://github.com/estuhub/airbnb-mvc" target="_blank" rel="noreferrer" className="project-link">
                         <div className="project-overlay">
                             <h3 className="project-title">Airbnb Clone</h3>
                             <p className="project-details">MVC - HTML - CSS - JavaScript - BootStrap - Express.js - Node.js - MongoDB - Passport</p>
@@ -25,7 +25,7 @@ class Projects extends React.Component{
                     </a>
                 </div>
                 <div className="project-container project-3">
-                    <a href="https://github.com/estuhub/follow-my-trail-react" target="_blank" rel="noreferrer" class="project-link">
+                    <a href="https://github.com/estuhub/follow-my-trail-react" target="_blank" rel="noreferrer" className="project-link">
                         <div className="project-overlay">
                             <h3 className="project-title">Follow My Trail</h3>
                             <p className="project-details">React - JSX - CSS - BootStrap - Node.js - GoogleMaps API - Axios - MongoDB</p>
