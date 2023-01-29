9dc55644308f23ed4f52eed1f1e54edd33b855d0
estuhub <107235863+estuhub@users.noreply.github.com>
2023-01-29 20:30:59 +0700

    create a ThemeContext using Hook
    + to toggle between dark-light mode

--- src/App.js
+++ src/App.js
@@ -6,23 +6,35 @@ import About from './pages/About';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
 
-// create component
-class App extends React.Component{
-  render() {
+// import React Hook
+import { createContext, useState } from 'react';
+
+// export ThemeContext
+export const ThemeContext = createContext("null");
+
+// create function
+function App() {
+    const [theme, setTheme] = useState("dark");
+    // create a toggle function where it adds "dark" to the #id of the <div> that wraps everything
+    const toggleTheme = () => {
+      setTheme((curr) => (curr === "" ? "dark" : ""))
+    };
     return (
-      <div>
-        <Nav />
-        <Home />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
+      <ThemeContext.Provider value={{ theme, toggleTheme}}>
+        {/* this theme will affect all the components below */}
+        <div id={ theme }>
+          <Nav />
+          <Home />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
     )
-  }
 }
 
 // export react
